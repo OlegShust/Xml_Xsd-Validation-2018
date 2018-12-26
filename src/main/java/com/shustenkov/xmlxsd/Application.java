ff2a042f3ba9b71cbbef3c7eec7688ef4afef72e
OlegShust <shus.oleg@gmail.com>
2018-12-26 23:05:59 +0300

    Update Application.java

--- src/main/java/com/shustenkov/xmlxsd/Application.java
+++ src/main/java/com/shustenkov/xmlxsd/Application.java
@@ -3,6 +3,9 @@ package com.shustenkov.xmlxsd;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Springboot - сервис проверки XML по XSD файлу
+ */
 @SpringBootApplication
 public class Application {
 
